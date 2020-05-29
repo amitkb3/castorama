93c2b497caf504403d3426498a49fc06dea370a0
Amit Bothra <amit.bothra@outlook.com>
2020-05-28 19:56:36 -0500

    created dump psql file for testing

--- test_app.py
+++ test_app.py
@@ -1,1 +1,15 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy 
 
+from app import app
+from models import db
+
+class CastingAgencyTestCase(unittest.TestCase):
+  """This class represents the casting agency test case"""
+
+  def setUp(self):
+    self.app = app
+    self.client = self.app.test_client
+    
