eaaafcb9e8078763c1e706dc4576e1fa0b40a50c
Amit Bothra <amit.bothra@outlook.com>
2020-05-28 20:41:10 -0500

    created first test

--- test_app.py
+++ test_app.py
@@ -12,4 +12,29 @@ class CastingAgencyTestCase(unittest.TestCase):
   def setUp(self):
     self.app = app
     self.client = self.app.test_client
-    
+    self.app = app
+    self.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URL')
+
+    self.new_actor = {
+      "name": "actor3",
+      "age": "25",
+      "gender":"F"
+    }
+  
+  def tearDown(self):
+    """Executed after reach test"""
+    pass
+
+  #  Success behavior tests
+  #  ----------------------------------------------------------------
+  def test_get_actors (self):
+    res = self.client().get('/actors')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertTrue(data['success'])
+    self.assertTrue(len(data['actors']) >= 0)
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+  unittest.main()
