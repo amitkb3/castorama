d691629e3058dc3115a702dc033aa809a9676a49
Amit Bothra <amit.bothra@outlook.com>
2020-05-29 15:45:46 -0500

    added test for success behavior

--- test_app.py
+++ test_app.py
@@ -1,10 +1,11 @@
 import os
 import unittest
 import json
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from app import app
-from models import db
+from models import db, Actor, Movie
 
 class CastingAgencyTestCase(unittest.TestCase):
   """This class represents the casting agency test case"""
@@ -20,6 +21,11 @@ class CastingAgencyTestCase(unittest.TestCase):
       "age": "25",
       "gender":"F"
     }
+
+    self.new_movie = {
+      "title": "movie3",
+      "release_date": datetime.utcnow()
+    }
   
   def tearDown(self):
     """Executed after reach test"""
@@ -28,12 +34,77 @@ class CastingAgencyTestCase(unittest.TestCase):
   #  Success behavior tests
   #  ----------------------------------------------------------------
   def test_get_actors (self):
+    """Test actors GET endpoint"""
     res = self.client().get('/actors')
     data = json.loads(res.data)
 
     self.assertEqual(res.status_code, 200)
     self.assertTrue(data['success'])
     self.assertTrue(len(data['actors']) >= 0)
+  
+  def test_get_movies (self):
+    """Test movies GET endpoint"""
+    res = self.client().get('/movies')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertTrue(data['success'])
+    self.assertTrue(len(data['movies']) >= 0)
+  
+  def test_add_new_actor(self):
+    """Test actors POST endpoint"""
+    res = self.client().post('/actors', json=self.new_actor)
+    data = json.loads(res.data)
+    self.assertEqual(res.status_code, 201)
+    self.assertEqual(data['success'], True)
+    self.assertEqual(data['actor']['name'],'actor3')
+  
+  def test_add_new_movie(self):
+    """Test movie POST endpoint"""
+    res = self.client().post('/movies', json=self.new_movie)
+    data = json.loads(res.data)
+    self.assertEqual(res.status_code, 201)
+    self.assertEqual(data['success'], True)
+    self.assertEqual(data['movie']['title'],'movie3')
+  
+  def test_edit_actor(self):
+    """Test actors PATCH endpoint"""
+    res = self.client().patch('/actors/1', json=self.new_actor)
+    data = json.loads(res.data)    
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertEqual(data['actor']['name'],'actor3')
+  
+  def test_edit_movie(self):
+    """Test movie PATCH endpoint"""
+    res = self.client().patch('/movies/1', json=self.new_movie)
+    data = json.loads(res.data)    
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertEqual(data['movie']['title'],'movie3')
+  
+  def test_delete_actor(self):
+    """Test actor DELETE endpoint"""
+    res = self.client().post('/actors', json=self.new_actor)
+    data = json.loads(res.data)
+    actor_id = data['actor']['id']
+    res_delete = self.client().delete(f'/actors/{actor_id}')
+    data_delete = json.loads(res_delete.data)    
+    self.assertEqual(res_delete.status_code, 200)
+    self.assertEqual(data_delete['success'], True)
+    self.assertEqual(data_delete['delete'], actor_id)
+  
+  def test_delete_movie(self):
+    """Test movie DELETE endpoint"""
+    res = self.client().post('/movies', json=self.new_movie)
+    data = json.loads(res.data)
+    movie_id = data['movie']['id']    
+    res_delete = self.client().delete(f'/movies/{movie_id}')
+    data_delete = json.loads(res_delete.data)    
+    self.assertEqual(res_delete.status_code, 200)
+    self.assertEqual(data_delete['success'], True)
+    self.assertEqual(data_delete['delete'], movie_id)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
