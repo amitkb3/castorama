09d79be5dd330a6d3fa2789c03f0e8b4267147c1
Amit Bothra <amit.bothra@outlook.com>
2020-05-30 15:22:10 -0500

    made code meet PEP 8 style guide

--- app.py
+++ app.py
@@ -8,263 +8,284 @@ from auth import AuthError, requires_auth
 
 # create and configure the app
 app = Flask(__name__)
-app.config.from_object(Config) # connect app to a local postgresql database
-db = SQLAlchemy(app) # initializing the instance with the app context
+app.config.from_object(Config)  # connect app to a local postgresql database
+db = SQLAlchemy(app)  # initializing the instance with the app context
 CORS(app)
 
 # ROUTES
 
 # Endpoint for index
+
+
 @app.route('/')
 def index():
-  return jsonify({
-    'app status': 'healthy'
-  }), 200
+    return jsonify({
+        'app status': 'healthy'
+    }), 200
 
 # Endpoint route handler for GET request for actor
+
+
 @app.route('/actors')
 @requires_auth('get:actor')
 def get_actors(payload):
-  """
-  Get details of all actors
-  :return details of all actors
-  """
-  try:
-    results = Actor.query.order_by(Actor.id).all()    
-    if len(results) == 0:
-      abort(404)
-    actors = [row.format() for row in results]    
-    return jsonify({
-      'success': True,
-      'actors': actors
-    }), 200
-  except Exception:
-    abort(422)
+    """
+    Get details of all actors
+    :return details of all actors
+    """
+    try:
+        results = Actor.query.order_by(Actor.id).all()
+        if len(results) == 0:
+            abort(404)
+        actors = [row.format() for row in results]
+        return jsonify({
+            'success': True,
+            'actors': actors
+        }), 200
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for GET request for movies
+
+
 @app.route('/movies')
 @requires_auth('get:movies')
 def get_movies(payload):
-  """
-  Get details of all movies
-  :return details of all movies
-  """
-  try:
-    results = Movie.query.all()
-    if len(results) == 0:
-      abort(404)
-    movies = [row.format() for row in results]
-    return jsonify({
-      'success': True,
-      'movies': movies
-    }), 200
-  except Exception:
-    abort(422)
+    """
+    Get details of all movies
+    :return details of all movies
+    """
+    try:
+        results = Movie.query.all()
+        if len(results) == 0:
+            abort(404)
+        movies = [row.format() for row in results]
+        return jsonify({
+            'success': True,
+            'movies': movies
+        }), 200
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for POST request for actor
+
+
 @app.route('/actors', methods=['POST'])
 @requires_auth('post:actor')
 def create_actor(payload):
-  """
-  Add new actor to database
-  :return newly added actor
-  """
-  body = request.get_json(request)
-  name = body.get('name')
-  age = body.get('age')
-  gender = body.get('gender')
-  if name is None or age is None or gender is None:
-    abort(400)
-  try:
-    new_actor = Actor(name=name, age=age, gender=gender)
-    new_actor.insert()
-    new_actor_formated = new_actor.format()
+    """
+    Add new actor to database
+    :return newly added actor
+    """
+    body = request.get_json(request)
+    name = body.get('name')
+    age = body.get('age')
+    gender = body.get('gender')
+    if name is None or age is None or gender is None:
+        abort(400)
+    try:
+        new_actor = Actor(name=name, age=age, gender=gender)
+        new_actor.insert()
+        new_actor_formated = new_actor.format()
 
-    # Return newly created actor
-    return jsonify({
-      'success': True,
-      'actor':new_actor_formated
-    }), 201
-  except Exception:
-    abort(422)
+        # Return newly created actor
+        return jsonify({
+            'success': True,
+            'actor': new_actor_formated
+        }), 201
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for POST request for movie
+
+
 @app.route('/movies', methods=['POST'])
 @requires_auth('post:Movies')
 def create_movie(payload):
-  """
-  Add new movie to database
-  :return newly added movie
-  """
-  body = request.get_json(request)
-  title = body.get('title')
-  release_date = body.get('release_date')
-  
-  if title is None or release_date is None:
-    abort(400) 
-  try:
-    new_movie = Movie(title=title, release_date=release_date)
-    new_movie.insert()
-    new_movie_formated = new_movie.format()
+    """
+    Add new movie to database
+    :return newly added movie
+    """
+    body = request.get_json(request)
+    title = body.get('title')
+    release_date = body.get('release_date')
 
-    # Return newly created movie
-    return jsonify({
-      'success': True,
-      'movie':new_movie_formated
-    }), 201
-  except Exception:
-    abort(422)
+    if title is None or release_date is None:
+        abort(400)
+    try:
+        new_movie = Movie(title=title, release_date=release_date)
+        new_movie.insert()
+        new_movie_formated = new_movie.format()
+
+        # Return newly created movie
+        return jsonify({
+            'success': True,
+            'movie': new_movie_formated
+        }), 201
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for PATCH request for actor
+
+
 @app.route('/actors/<int:actor_id>', methods=['PATCH'])
 @requires_auth('patch:actor')
 def update_actor(payload, actor_id):
-  """
-  Edit information for given actor id
-  :return updated actor information
-  """
-  actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
-  if actor is None:
-    abort(404)
+    """
+    Edit information for given actor id
+    :return updated actor information
+    """
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    if actor is None:
+        abort(404)
 
-  body = request.get_json(request)
-  name = body.get('name', None)
-  age = body.get('age', None)
-  gender = body.get('gender',None)
-  if name is None or age is None or gender is None:
-    abort(400)  
-  try:
-    actor.name = name
-    actor.age = age
-    actor.gender = gender
-    actor.update()
-    actor_formated = actor.format()
+    body = request.get_json(request)
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+    if name is None or age is None or gender is None:
+        abort(400)
+    try:
+        actor.name = name
+        actor.age = age
+        actor.gender = gender
+        actor.update()
+        actor_formated = actor.format()
 
-    # Return edited actor
-    return jsonify({
-      'success': True,
-      'actor':actor_formated
-    }), 200
-  except Exception:
-    abort(422)
+        # Return edited actor
+        return jsonify({
+            'success': True,
+            'actor': actor_formated
+        }), 200
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for PATCH request for movie
+
+
 @app.route('/movies/<int:movie_id>', methods=['PATCH'])
 @requires_auth('patch:Movies')
 def update_movie(payload, movie_id):
-  """
-  Edit information for given movie id
-  :return updated movie information
-  """
-  movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-  if movie is None:
-    abort(404)
+    """
+    Edit information for given movie id
+    :return updated movie information
+    """
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+    if movie is None:
+        abort(404)
 
-  body = request.get_json(request)
-  title = body.get('title')
-  release_date = body.get('release_date')
-  
-  if title is None or release_date is None:
-    abort(400)
-  try:
-    movie.title = title
-    movie.release_date = release_date
-    movie.update()
-    movie_formated = movie.format()
+    body = request.get_json(request)
+    title = body.get('title')
+    release_date = body.get('release_date')
 
-    # Return edited movie
-    return jsonify({
-      'success': True,
-      'movie':movie_formated
-    }), 200
-  except Exception:
-    abort(422)
+    if title is None or release_date is None:
+        abort(400)
+    try:
+        movie.title = title
+        movie.release_date = release_date
+        movie.update()
+        movie_formated = movie.format()
+
+        # Return edited movie
+        return jsonify({
+            'success': True,
+            'movie': movie_formated
+        }), 200
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for DELETE request for actor
+
+
 @app.route('/actors/<int:actor_id>', methods=['DELETE'])
 @requires_auth('delete:actor')
 def delete_actor(payload, actor_id):
-  """
-  Delete actor using actor id
-  :return id of actor being deleted
-  """
-  actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
-  if actor is None:
-    abort(404)
-  try:
-    actor.delete()
-  # Return deleted actor id
-    return jsonify({
-      'success': True,
-      'delete':actor_id
-    }), 200
-  except Exception:
-    abort(422)
+    """
+    Delete actor using actor id
+    :return id of actor being deleted
+    """
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    if actor is None:
+        abort(404)
+    try:
+        actor.delete()
+    # Return deleted actor id
+        return jsonify({
+            'success': True,
+            'delete': actor_id
+        }), 200
+    except Exception:
+        abort(422)
 
 # Endpoint route handler for DELETE request for movie
+
+
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
 @requires_auth('delete:movies')
 def delete_movie(payload, movie_id):
-  """
-  Delete movie using movie id
-  :return id of movie being deleted
-  """
-  movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-  if movie is None:
-    abort(404)
-  try:
-    movie.delete()
-  # Return deleted movie id
-    return jsonify({
-      'success': True,
-      'delete':movie_id
-    }), 200
-  except Exception:
-    abort(422)
+    """
+    Delete movie using movie id
+    :return id of movie being deleted
+    """
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+    if movie is None:
+        abort(404)
+    try:
+        movie.delete()
+    # Return deleted movie id
+        return jsonify({
+            'success': True,
+            'delete': movie_id
+        }), 200
+    except Exception:
+        abort(422)
 
 # Error handling
 
 
 @app.errorhandler(422)
 def unprocessable(error):
-  """
-  Unprocessable Entity
-  """
-  return jsonify({
-    "success": False,
-    "error": 422,
-    "message": "unprocessable"
-  }), 422
+    """
+    Unprocessable Entity
+    """
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
 
 @app.errorhandler(404)
 def not_found(error):
-  """
-  Not Found
-  """
-  return jsonify({
-    "success": False,
-    "error": 404,
-    "message": "not found"
-  }), 404
+    """
+    Not Found
+    """
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "not found"
+    }), 404
+
 
 @app.errorhandler(400)
 def bad_request(error):
-  """
-  Bad request
-  """
-  return jsonify({
-    "success": False,
-    "error": 400,
-    "message": "bad request"
-  }), 400
-    
-# AuthError exceptions raised by the @requires_auth(permission) decorator method
+    """
+    Bad request
+    """
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+# AuthError exceptions raised by the
+# @requires_auth(permission) decorator method
+
+
 @app.errorhandler(AuthError)
 def auth_error(auth_error):
-  return jsonify({
-    "success": False,
-    "error": auth_error.status_code,
-    "message": auth_error.error['description']
-  }), auth_error.status_code
-    
-  
+    return jsonify({
+        "success": False,
+        "error": auth_error.status_code,
+        "message": auth_error.error['description']
+    }), auth_error.status_code
--- test_app.py
+++ test_app.py
@@ -11,168 +11,189 @@ assistant_token = "Bearer {}".format(os.environ.get('ASSISTANT_JWT'))
 director_token = "Bearer {}".format(os.environ.get('DIRECTOR_JWT'))
 producer_token = "Bearer {}".format(os.environ.get('PRODUCER_JWT'))
 
+
 class CastingAgencyTestCase(unittest.TestCase):
-  """This class represents the casting agency test case"""
+    """This class represents the casting agency test case"""
 
-  def setUp(self):
-    self.app = app
-    self.client = self.app.test_client
-    self.app = app
-    self.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URL')
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client
+        self.app = app
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            'TEST_DATABASE_URL')
 
-    self.new_actor = {
-      "name": "actor3",
-      "age": "25",
-      "gender":"F"
-    }
+        self.new_actor = {
+            "name": "actor3",
+            "age": "25",
+            "gender": "F"
+        }
 
-    self.new_movie = {
-      "title": "movie3",
-      "release_date": datetime.utcnow()
-    }
-  
-  def tearDown(self):
-    """Executed after reach test"""
-    pass
+        self.new_movie = {
+            "title": "movie3",
+            "release_date": datetime.utcnow()
+        }
 
-  #  Success behavior tests
-  #  ----------------------------------------------------------------
-  def test_get_actors (self):
-    """Test actors GET endpoint"""
-    res = self.client().get('/actors', headers={"Authorization": (assistant_token)})
-    data = json.loads(res.data)
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
 
-    self.assertEqual(res.status_code, 200)
-    self.assertTrue(data['success'])
-    self.assertTrue(len(data['actors']) >= 0)
-  
-  def test_get_movies (self):
-    """Test movies GET endpoint"""
-    res = self.client().get('/movies', headers={"Authorization": (director_token)})
-    data = json.loads(res.data)
+    #  Success behavior tests
+    #  ----------------------------------------------------------------
+    def test_get_actors(self):
+        """Test actors GET endpoint"""
+        res = self.client().get(
+            '/actors', headers={"Authorization": (assistant_token)})
+        data = json.loads(res.data)
 
-    self.assertEqual(res.status_code, 200)
-    self.assertTrue(data['success'])
-    self.assertTrue(len(data['movies']) >= 0)
-  
-  def test_add_new_actor(self):
-    """Test actors POST endpoint"""
-    res = self.client().post('/actors', json=self.new_actor, headers={"Authorization": (director_token)})
-    data = json.loads(res.data)
-    self.assertEqual(res.status_code, 201)
-    self.assertEqual(data['success'], True)
-    self.assertEqual(data['actor']['name'],'actor3')
-  
-  def test_add_new_movie(self):
-    """Test movie POST endpoint"""
-    res = self.client().post('/movies', json=self.new_movie, headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)
-    self.assertEqual(res.status_code, 201)
-    self.assertEqual(data['success'], True)
-    self.assertEqual(data['movie']['title'],'movie3')
-  
-  def test_edit_actor(self):
-    """Test actors PATCH endpoint"""
-    res = self.client().patch('/actors/1', json=self.new_actor, headers={"Authorization": (director_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 200)
-    self.assertEqual(data['success'], True)
-    self.assertEqual(data['actor']['name'],'actor3')
-  
-  def test_edit_movie(self):
-    """Test movie PATCH endpoint"""
-    res = self.client().patch('/movies/1', json=self.new_movie, headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 200)
-    self.assertEqual(data['success'], True)
-    self.assertEqual(data['movie']['title'],'movie3')
-  
-  def test_delete_actor(self):
-    """Test actor DELETE endpoint"""
-    res = self.client().post('/actors', json=self.new_actor, headers={"Authorization": (director_token)})
-    data = json.loads(res.data)
-    actor_id = data['actor']['id']
-    res_delete = self.client().delete(f'/actors/{actor_id}', headers={"Authorization": (director_token)})
-    data_delete = json.loads(res_delete.data)    
-    self.assertEqual(res_delete.status_code, 200)
-    self.assertEqual(data_delete['success'], True)
-    self.assertEqual(data_delete['delete'], actor_id)
-  
-  def test_delete_movie(self):
-    """Test movie DELETE endpoint"""
-    res = self.client().post('/movies', json=self.new_movie, headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)
-    movie_id = data['movie']['id']    
-    res_delete = self.client().delete(f'/movies/{movie_id}', headers={"Authorization": (producer_token)})
-    data_delete = json.loads(res_delete.data)    
-    self.assertEqual(res_delete.status_code, 200)
-    self.assertEqual(data_delete['success'], True)
-    self.assertEqual(data_delete['delete'], movie_id)
-  
-  #  bad behavior tests
-  # ----------------------------------------------------------------
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(len(data['actors']) >= 0)
 
-  def test_unauthorized_get_actors (self):
-    """Test actors GET endpoint without authorization"""
-    res = self.client().get('/actors', headers={"Authorization": ()})
-    data = json.loads(res.data)
+    def test_get_movies(self):
+        """Test movies GET endpoint"""
+        res = self.client().get(
+            '/movies', headers={"Authorization": (director_token)})
+        data = json.loads(res.data)
 
-    self.assertEqual(res.status_code, 401)
-    self.assertFalse(data['success'])
-  
-  def test_unauthorized_get_movies (self):
-    """Test movies GET endpoint without authorization"""
-    res = self.client().get('/movies')
-    data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(len(data['movies']) >= 0)
 
-    self.assertEqual(res.status_code, 401)
-    self.assertFalse(data['success'])
-  
-  def test_assistant_add_new_actor(self):
-    """Test actors POST endpoint with assistant role"""
-    res = self.client().post('/actors', json=self.new_actor, headers={"Authorization": (assistant_token)})
-    data = json.loads(res.data)
-    self.assertEqual(res.status_code, 403)
-    self.assertFalse(data['success'], True)
-  
-  def test_no_data_add_new_movie(self):
-    """Test movie POST endpoint without data"""
-    res = self.client().post('/movies', json={}, headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 400)
-    self.assertFalse(data['success'], True)
+    def test_add_new_actor(self):
+        """Test actors POST endpoint"""
+        res = self.client().post('/actors', json=self.new_actor,
+                                 headers={"Authorization": (director_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['actor']['name'], 'actor3')
 
-  def test_unauthorized_edit_actor(self):
-    """Test actors PATCH endpoint with unauthorized assistant role"""
-    res = self.client().patch('/actors/1', json=self.new_actor, headers={"Authorization": (assistant_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 403)
-    self.assertFalse(data['success'], True)
+    def test_add_new_movie(self):
+        """Test movie POST endpoint"""
+        res = self.client().post('/movies', json=self.new_movie,
+                                 headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['movie']['title'], 'movie3')
 
-  def test_no_data_edit_movie(self):
-    """Test movie PATCH endpoint with no data"""
-    res = self.client().patch('/movies/1', json={}, headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 400)
-    self.assertFalse(data['success'], True)
+    def test_edit_actor(self):
+        """Test actors PATCH endpoint"""
+        res = self.client().patch('/actors/1', json=self.new_actor,
+                                  headers={"Authorization": (director_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['actor']['name'], 'actor3')
 
-  def test_unauthorized_delete_actor(self):
-    """Test actor DELETE endpoint with unauthorized assistant role"""
-    res = self.client().post('/actors', json=self.new_actor, headers={"Authorization": (director_token)})
-    data = json.loads(res.data)
-    actor_id = data['actor']['id']
-    res_delete = self.client().delete(f'/actors/{actor_id}', headers={"Authorization": (assistant_token)})
-    data_delete = json.loads(res_delete.data)    
-    self.assertEqual(res_delete.status_code, 403)
-    self.assertFalse(data_delete['success'], True)
+    def test_edit_movie(self):
+        """Test movie PATCH endpoint"""
+        res = self.client().patch('/movies/1', json=self.new_movie,
+                                  headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['movie']['title'], 'movie3')
 
-  def test_no_data_delete_movie(self):
-    """Test movie DELETE endpoint"""        
-    res = self.client().delete('/movies/1000', headers={"Authorization": (producer_token)})
-    data = json.loads(res.data)    
-    self.assertEqual(res.status_code, 404)
-    self.assertFalse(data['success'], True)        
+    def test_delete_actor(self):
+        """Test actor DELETE endpoint"""
+        res = self.client().post('/actors', json=self.new_actor,
+                                 headers={"Authorization": (director_token)})
+        data = json.loads(res.data)
+        actor_id = data['actor']['id']
+        res_delete = self.client().delete(
+            f'/actors/{actor_id}', headers={"Authorization": (director_token)})
+        data_delete = json.loads(res_delete.data)
+        self.assertEqual(res_delete.status_code, 200)
+        self.assertEqual(data_delete['success'], True)
+        self.assertEqual(data_delete['delete'], actor_id)
+
+    def test_delete_movie(self):
+        """Test movie DELETE endpoint"""
+        res = self.client().post('/movies', json=self.new_movie,
+                                 headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        movie_id = data['movie']['id']
+        res_delete = self.client().delete(
+            f'/movies/{movie_id}', headers={"Authorization": (producer_token)})
+        data_delete = json.loads(res_delete.data)
+        self.assertEqual(res_delete.status_code, 200)
+        self.assertEqual(data_delete['success'], True)
+        self.assertEqual(data_delete['delete'], movie_id)
+
+    #  bad behavior tests
+    # ----------------------------------------------------------------
+
+    def test_unauthorized_get_actors(self):
+        """Test actors GET endpoint without authorization"""
+        res = self.client().get('/actors', headers={"Authorization": ()})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data['success'])
+
+    def test_unauthorized_get_movies(self):
+        """Test movies GET endpoint without authorization"""
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data['success'])
+
+    def test_assistant_add_new_actor(self):
+        """Test actors POST endpoint with assistant role"""
+        res = self.client().post('/actors', json=self.new_actor,
+                                 headers={"Authorization": (assistant_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 403)
+        self.assertFalse(data['success'], True)
+
+    def test_no_data_add_new_movie(self):
+        """Test movie POST endpoint without data"""
+        res = self.client().post('/movies', json={},
+                                 headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'], True)
+
+    def test_unauthorized_edit_actor(self):
+        """Test actors PATCH endpoint with unauthorized assistant role"""
+        res = self.client().patch('/actors/1', json=self.new_actor,
+                                  headers={"Authorization": (assistant_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 403)
+        self.assertFalse(data['success'], True)
+
+    def test_no_data_edit_movie(self):
+        """Test movie PATCH endpoint with no data"""
+        res = self.client().patch('/movies/1', json={},
+                                  headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'], True)
+
+    def test_unauthorized_delete_actor(self):
+        """Test actor DELETE endpoint with unauthorized assistant role"""
+        res = self.client().post('/actors', json=self.new_actor,
+                                 headers={"Authorization": (director_token)})
+        data = json.loads(res.data)
+        actor_id = data['actor']['id']
+        res_delete = self.client().delete(
+            f'/actors/{actor_id}',
+            headers={"Authorization": (assistant_token)})
+        data_delete = json.loads(res_delete.data)
+        self.assertEqual(res_delete.status_code, 403)
+        self.assertFalse(data_delete['success'], True)
+
+    def test_no_data_delete_movie(self):
+        """Test movie DELETE endpoint"""
+        res = self.client().delete(
+            '/movies/1000', headers={"Authorization": (producer_token)})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'], True)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
